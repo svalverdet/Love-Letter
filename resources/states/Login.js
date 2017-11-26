daf3d818c3c167a34dbdd970424672b810091810
SaraLopGar <32269870+SaraLopGar@users.noreply.github.com>
2017-11-26 02:22:10 +0100

    Inicio Implementación Login

--- resources/states/Login.js
+++ resources/states/Login.js
@@ -9,6 +9,11 @@ LoveLetterOnline.Login.prototype = {
 		texto = this.add.text(this.world.centerX, this.world.centerY,'Entrar',{fill: "#ffffff"});
 		texto.inputEnabled = true;
 		texto.events.onInputDown.add(this.entrar, this);
+		
+		//Se crea el cuadro para poner el nombre del jugador
+		$('#info').append(
+        '<div id="input-value"><input type="text"></div>');
+		
 	},
 	entrar: function(){
 		this.state.start('Menu');
