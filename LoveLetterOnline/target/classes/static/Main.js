13e361f3069cf40dd6ab007ff61ec410f603c140
svalverdet <32246322+svalverdet@users.noreply.github.com>
2017-11-26 12:47:51 +0100

    Add files via upload

---
+++ LoveLetterOnline/target/classes/static/Main.js
@@ -0,0 +1,15 @@
+var gameOptions = {
+    gameWidth: 1280,
+    gameHeight: 720,
+    cardSheetWidth: 178,
+    cardSheetHeight: 208
+};
+
+var game = new Phaser.Game(gameOptions.gameWidth, gameOptions.gameHeight, Phaser.AUTO);
+game.state.add('Menu', LoveLetterOnline.Menu);
+game.state.add('Login', LoveLetterOnline.Login);
+
+game.state.add('Jugar', LoveLetterOnline.Jugar);
+game.state.add('Ranking', LoveLetterOnline.Ranking);
+
+game.state.start('Login');
